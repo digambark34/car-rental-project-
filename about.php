3dd4b150e3c2a1be288062521c834ed6d981729b
Digambar <digambarkothawale05@gmail.com>
2024-01-10 22:07:32 +0530

    Car Rental System

---
+++ about.php
@@ -0,0 +1,94 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Car Rental | About us</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  <meta content="Author" name="WebThemez">
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet"> 
+</head>
+
+<body id="body"> 
+ <?php include('includes/header.php');?>
+ <section id="innerBanner"> 
+  <div class="inner-content">
+    <h2><span>ABOUT US</span><br>We provide high quality cars</h2>
+    <div> 
+    </div>
+  </div> 
+</section><!-- #Page Banner -->
+
+<main id="main">
+      <section id="about" class="wow fadeInUp">
+        <div class="container"> 
+          <div class="row">
+            <div class="col-lg-6 about-img">
+              <img src="img/hack1.png" alt="">
+            </div>
+
+            <div class="col-lg-6 content">
+              <h2> <b>This project was developed by a team of 5 members :- </b></h2>
+                <li><i class="icon ion-ios-checkmark-outline"></i> DIGAMBAR BABALI KOTHAWALE.</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i> SARTH UMESH MATHKAR.</li>
+                <li><i class="icon ion-ios-checkmark-outline"></i> RAMCHANDRA VIJAY GAWADE.</li>
+                 <li><i class="icon ion-ios-checkmark-outline"></i> TEJAS SHIVPRASAD BANDEKAR.</li>
+                  <li><i class="icon ion-ios-checkmark-outline"></i> DNYANESH AMOL PRABHUZANTYE.</li>
+                  <br>
+                <h3> These 5 people fought hard for all customers to have a great experience while using this website.
+Thank you.....
+</h3>
+              </ul> 
+            </div>
+          </div>
+
+        </div>
+      </section><!-- #about -->
+    </main>
+    <?php include('includes/footer.php');?>
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- JavaScript  -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/superfish/hoverIntent.js"></script>
+    <script src="lib/superfish/superfish.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+    <script src="lib/sticky/sticky.js"></script> 
+    <script src="contact/jqBootstrapValidation.js"></script>
+    <script src="contact/contact_me.js"></script>
+    <script src="js/main.js"></script>
+
+  </body>
+  </html>
---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+
+<footer class="footer">
+	<div class="d-sm-flex justify-content-center justify-content-sm-between">
+		<span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021-22 <a href="https://www.sybespolytechnic.com/" target="_blank">YASHWANTRAO BHONSALE POLYTECHNIC SAWANTWADI </a>. All rights reserved.</span>
+        <span class="text-muted float-none float-sm-right d-block mt-1 mt-sm-0 text-center pr-3">Powered by:  BKC STUDENTS </span>
+    </div>
+</footer>
